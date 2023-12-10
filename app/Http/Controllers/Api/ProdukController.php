6ecd95196a1c3a81884fe1def7f3fc3dfafdace5
wulandarikaranamu02@gmail.com <wulandarikaranamu02@gmail.com>
2023-12-10 10:32:06 -0800

    api scantum

--- app/Http/Controllers/Api/ProdukController.php
+++ app/Http/Controllers/Api/ProdukController.php
@@ -8,6 +8,7 @@ use DB;
 use App\Models\Produk;
 use App\Models\Jenis_produk;
 use App\Http\Resources\ProdukResource;
+use Illuminate\Support\Facades\Validator;
 
 class ProdukController extends Controller
 {
@@ -19,4 +20,73 @@ class ProdukController extends Controller
         return new ProdukResource(true,'Data Produk', $produk);
 
     }
-}
+    public function show($id){
+        $produk = Produk::join('jenis_produk', 'jenis_produk_id', '=', 'jenis_produk.id')
+        ->select('produk.*', 'jenis_produk.nama as jenis')
+        ->where('produk.id', $id)
+        ->get();
+        return new ProdukResource(true,'Detail Produk', $produk);
+    
+    }
+    public function store(Request $request){
+        // $request->validate([
+            $validator = Validator::make($request->all(),[
+            'kode' => 'required|unique:produk|max:10',
+            'nama' => 'required|max:45',
+            'harga_beli' => 'required|numeric',
+            'harga_jual' => 'required|numeric',
+            'stok' => 'required|numeric',
+            'min_stok' => 'required|numeric',
+            'deksripsi' => 'nullable|string|min:10',
+            'jenis_produk_id' => 'required|integer',
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors(),422);
+        }
+            DB::table('produk')->insert([
+                'kode'=>$request->kode,
+                'nama'=>$request->nama,
+                'harga_beli'=>$request->harga_beli,
+                'harga_jual'=>$request->harga_jual,
+                'stok'=>$request->stok,
+                'min_stok'=>$request->min_stok,
+                'foto'=>$request->foto,
+                'deskripsi'=>$request->deskripsi,
+                'jenis_produk_id'=>$request->jenis_produk_id,
+            ]);
+            return new ProdukResource(true,'Data Produk berhasil ditambahkan','produk');
+        }
+        public function update(Request $request, $id){
+            $validator = Validator::make($request->all(),[
+                'kode' => 'required|max:10',
+                'nama' => 'required|max:45',
+                'harga_beli' => 'required|numeric',
+                'harga_jual' => 'required|numeric',
+                'stok' => 'required|numeric',
+                'min_stok' => 'required|numeric',
+                'deksripsi' => 'nullable|string|min:10',
+                'jenis_produk_id' => 'required|integer',
+            ]);
+            if($validator->fails()){
+                return response()->json($validator->errors(),422);
+            }
+            $produk = Produk::whereId($id)->update([
+                'kode'=>$request->kode,
+                'nama'=>$request->nama,
+                'harga_beli'=>$request->harga_beli,
+                'harga_jual'=>$request->harga_jual,
+                'stok'=>$request->stok,
+                'min_stok'=>$request->min_stok,
+                'foto'=>$request->foto,
+                'deskripsi'=>$request->deskripsi,
+                'jenis_produk_id'=>$request->jenis_produk_id,
+                ]);
+                return new ProdukResource(true,'Data Berhasil diubah',$produk);
+        }
+        public function destroy($id){
+            $produk = Produk::whereId($id)->first();
+            $produk->delete();
+            return new ProdukResource(true,'Data Berhasil Dihapus', $produk);
+        }
+    }
+
